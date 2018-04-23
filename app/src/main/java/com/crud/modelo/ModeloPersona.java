a8d21a6e942cc66dfd1163fad5bc1ac7367148f4
Maria Isabel Arcila Franco <maria.arcilaf@upb.edu.co>
2018-04-22 21:50:17 -0500

    Modelo persona y actividad registro
    Todavía no funciona

--- app/src/main/java/com/crud/modelo/ModeloPersona.java
+++ app/src/main/java/com/crud/modelo/ModeloPersona.java
@@ -1,8 +1,48 @@
 package com.crud.modelo;
 
+import com.crud.entities.Evento;
+import com.crud.entities.Persona;
+
+import org.json.JSONObject;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by gfghfh on 22/04/2018.
  */
 
 public class ModeloPersona {
+
+    private String BASE_URL = "http://venalau.azurewebsites.net/api/persona/";
+    private RestTemplate restTemplate = new RestTemplate();
+
+    public boolean create(Persona datos){
+        try{
+            Map<String,String> values = new HashMap<String,String>();
+            values.put("IdPersona", String.valueOf(datos.getIdPersona()));
+            values.put("TipoVinculo", datos.getTipoVinculo());
+            values.put("Nombre", datos.getNombre());
+            values.put("Apellido", datos.getApellido());
+            values.put("Correo", datos.getCorreo());
+            values.put("Contraseña", datos.getContraseña());
+            values.put("CantidadMultas", String.valueOf(datos.getCantidadMultas()));
+            //values.put("edad", String.valueOf(datos.getEdad()));
+            JSONObject jsonObject = new JSONObject(values);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<String> entity = new HttpEntity<String>(jsonObject.toString(), headers);
+            restTemplate.postForEntity(BASE_URL + "create", entity, null);
+            return true;
+        } catch(Exception e){
+            return false;
+        }
+    }
 }
--- app/src/main/java/com/example/sergiorodriguez/venalau_app/RegistroActivity.java
+++ app/src/main/java/com/example/sergiorodriguez/venalau_app/RegistroActivity.java
@@ -1,13 +1,94 @@
 package com.example.sergiorodriguez.venalau_app;
 
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.crud.entities.Persona;
+import com.crud.modelo.ModeloPersona;
 
 public class RegistroActivity extends AppCompatActivity {
 
+    EditText nombrePersona;
+    EditText apellidoPersona;
+    EditText idInstitucional;
+    EditText correoInstitucional;
+    EditText contraseña;
+    EditText confirmarContraseña;
+    Button botonRegistrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registro);
+
+
+        try{
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_registro);
+
+            nombrePersona=(EditText) findViewById(R.id.txtNombreRegistro);
+            apellidoPersona=(EditText) findViewById(R.id.txtApellidoRegistro);
+            idInstitucional=(EditText) findViewById(R.id.txtIDRegistro);
+            correoInstitucional=(EditText) findViewById(R.id.txtCorreoRegistro);
+            contraseña=(EditText) findViewById(R.id.txtContraseñaRegistro);
+            confirmarContraseña=(EditText) findViewById(R.id.txtConfirmarRegistro);
+            botonRegistrar=(Button) findViewById(R.id.btnRegistrarse);
+
+        }catch (Exception e){
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+            builder.setMessage(e.getMessage());
+            builder.create().show();
+        }
+
+
+    }
+
+    public void OnClickbtnRegistrarse(View v){
+        try{
+            Persona datos = new Persona();
+            datos.setNombre(nombrePersona.getText().toString());
+            datos.setApellido(apellidoPersona.getText().toString());
+            datos.setIdPersona(Integer.parseInt(idInstitucional.getText().toString()));
+            datos.setCorreo(correoInstitucional.getText().toString());
+            datos.setContraseña(contraseña.getText().toString());
+
+            boolean result = new HttpRequestAdd().execute(datos).get();
+            if(result){
+                Intent intent = new Intent(RegistroActivity.this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                builder.setMessage("Failed");
+                builder.create().show();
+            }
+
+        }catch (Exception e){
+            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+            builder.setMessage(e.getMessage());
+            builder.create().show();
+        }
+
+    }
+
+    private class HttpRequestAdd extends AsyncTask<Persona, Void, Boolean> {
+
+
+        @Override
+        protected Boolean doInBackground(Persona... datos) {
+
+            ModeloPersona datosModel = new ModeloPersona();
+
+            return datosModel.create(datos[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+        }
     }
 }
